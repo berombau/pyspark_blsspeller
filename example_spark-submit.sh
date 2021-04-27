f7ced76a16831c172d5916ffb923b1a904fa1c25
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2021-04-27 15:46:03 +0200

    use exec env in example

--- example_spark-submit.sh
+++ example_spark-submit.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# From http://spark.apache.org/docs/latest/api/python/user_guide/python_packaging.html
+# export PYSPARK_DRIVER_PYTHON=python # Do not set in cluster modes.
+# export PYSPARK_PYTHON=./environment/bin/python
+# --archives pyspark_conda_env.tar.gz#environment \
+
+export PYSPARK_DRIVER_PYTHON=~/miniconda3/envs/pyspark_blsspeller/bin/python
+export PYSPARK_PYTHON=~/miniconda3/envs/executor_pyspark_blsspeller/bin/python
 time spark-submit \
 --driver-memory 10G \
 --py-files dist/blsspeller-1.0-py2.py3-none-any.whl blsspeller/main.py \
