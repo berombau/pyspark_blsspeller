3d36ae75c5c381224c4ccba452bd77a59bbbe02f
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2021-04-27 10:53:59 +0200

    use exec instead of bindir

--- example_spark-submit.sh
+++ example_spark-submit.sh
@@ -5,6 +5,6 @@ time spark-submit \
 --py-files dist/blsspeller-1.0-py2.py3-none-any.whl blsspeller/main.py \
 --input ../input_analysis/subset \
 --output test_output \
---bindir ../suffixtree-motif-speller/motifIterator/build_parquet \
+--exec ../suffixtree-motif-speller/motifIterator/build_parquet/motifIterator \
 --limit 1 \
 |& tee output.log
