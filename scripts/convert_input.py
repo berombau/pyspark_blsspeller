77604a7a81c157cceeab4cff8c70d2a49dad0ed1
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2021-05-03 14:16:16 +0000

    refactor for doctest; add clean_seq

--- scripts/convert_input.py
+++ scripts/convert_input.py
@@ -5,25 +5,9 @@ import sys
 import logging
 import sys
 from collections import defaultdict
+import random
 
-parser = argparse.ArgumentParser(
-    description="Convert BLSpeller input files, changing Newick tree format from gene-centric to species-centric. Defaults to stdin and stdout."
-)
-parser.add_argument("-i", "--input", type=str, help="Input directory with files.")
-parser.add_argument(
-    "-o", "--output", type=str, help="Output directory to be created/overwritten."
-)
-parser.add_argument(
-    "-v", "--verbose", action='store_true', help="Set logging level to DEBUG."
-)
-args = parser.parse_args()
-
-if args.verbose:
-    level=logging.DEBUG
-else:
-    level=logging.WARNING
-
-logging.basicConfig(stream=sys.stderr, level=level)
+random.seed(123)
 
 def convert_tree(tree, genes):
     """
@@ -52,6 +36,43 @@ def new_genes_format(genes):
         for species_id, (ids, seqs) in genes_by_species.items()
     ]
 
+poss_dict = {
+    "A" : "A",
+    "C" : "C",
+    "G" : "G",
+    "T" : "T",
+    "N" : "ACGT",
+    "R" : "AG",
+    "Y" : "CT",
+    "S" : "GC",
+    "W" : "AT",
+    "K" : "GT",
+    "M" : "AC",
+    "B" : "CGT",
+    "D" : "AGT",
+    "H" : "ACT",
+    "V" : "ACG"
+}
+
+def clean_seq(seq):
+    """
+    >>> clean_seq('ACGT') 
+    'ACGT'
+    >>> clean_seq('NNNN')
+    'NNNN'
+    >>> clean_seq('ACGKAA')
+    'ACGGAA'
+    """
+    output = []
+    for c in seq:
+        if c in 'ACGTN':
+            output.append(c)
+        else:
+            poss = poss_dict[c]
+            choice = random.choice(poss)
+            output.append(choice)
+    return "".join(output)
+
 def act_on_input(f, o):
     family_id = f.readline().rstrip("\n")
     while family_id:
@@ -63,6 +84,7 @@ def act_on_input(f, o):
         for _ in range(num_genes):
             gene_id, species_id = f.readline().rstrip("\n").split()
             sequence = f.readline().rstrip("\n")
+            sequence = clean_seq(sequence)
             species.add(species_id)
             genes.append((gene_id, species_id, sequence))
         logging.info(species)
@@ -78,25 +100,46 @@ def act_on_input(f, o):
         )
         family_id = f.readline().rstrip("\n")
 
-if not args.input or args.input == "-":
-    # only 1 input, via std channels
-    act_on_input(sys.stdin, sys.stdout)
-    sys.exit()
-# assume input folder and output folder
-output = Path(args.output)
-if output.exists():
-    for f in output.iterdir():
-        f.unlink()
-    output.rmdir()
-output.mkdir()
 
-for file in Path(args.input).iterdir():
-    try:
-        with file.open(mode="r") as f:
-            output_path = output / file.name
-            output_path.touch()
-            with output_path.open(mode="w") as o:
-                act_on_input(f, o)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Convert BLSpeller input files, changing Newick tree format from gene-centric to species-centric. Defaults to stdin and stdout."
+    )
+    parser.add_argument("-i", "--input", type=str, help="Input directory with files.")
+    parser.add_argument(
+        "-o", "--output", type=str, help="Output directory to be created/overwritten."
+    )
+    parser.add_argument(
+        "-v", "--verbose", action='store_true', help="Set logging level to DEBUG."
+    )
+    args = parser.parse_args()
 
-    except Exception as e:
-        logging.error(f"Failed for file {file.name}. Caused by {e}")
+    if args.verbose:
+        level=logging.DEBUG
+    else:
+        level=logging.WARNING
+
+    logging.basicConfig(stream=sys.stderr, level=level)
+
+    if not args.input or args.input == "-":
+        # only 1 input, via std channels
+        act_on_input(sys.stdin, sys.stdout)
+        sys.exit()
+    # assume input folder and output folder
+    output = Path(args.output)
+    if output.exists():
+        for f in output.iterdir():
+            f.unlink()
+        output.rmdir()
+    output.mkdir()
+
+    for file in Path(args.input).iterdir():
+        try:
+            with file.open(mode="r") as f:
+                output_path = output / file.name
+                output_path.touch()
+                with output_path.open(mode="w") as o:
+                    act_on_input(f, o)
+
+        except Exception as e:
+            logging.error(f"Failed for file {file.name}. Caused by {e}")
